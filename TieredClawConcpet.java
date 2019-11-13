bd6874e73b1fcf59bba9c5a1c4d31194e32aaf44
bchspetersonowen <petersonowen20@bereanchristian.com>
2019-11-12 18:53:35 -0800

    new concept

---
+++ TieredClawConcpet.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="Claw Test")
+public class TieredClawConcpet {
+    Double Level
+
+    public void moveClawUpTier () {
+        //code that moves the claw up one tier
+    }
+
+}
