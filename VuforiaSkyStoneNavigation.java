98204d7e7364d29899817d27cdb93126ec5008ee
bchspetersonowen <petersonowen20@bereanchristian.com>
2019-11-16 11:04:23 -0800

    Upgraded stopping area by twice

--- VuforiaSkyStoneNavigation.java
+++ VuforiaSkyStoneNavigation.java
@@ -91,7 +91,7 @@ public class VuforiaSkyStoneNavigation extends LinearOpMode {
     private static final float quadField  = 36 * mmPerInch;
 
     //Constants for autonomous
-    private static final double mmTolerance = 50;
+    private static final double mmTolerance = 100;
     private static final double radianTolerance = (Math.PI/180);
     private static final double angleTolerance = 1;
 
