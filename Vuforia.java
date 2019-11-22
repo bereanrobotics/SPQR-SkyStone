185591fdae23b2c1ac301ea80edcca924a8efb92
Owen Peterson <petersonowen20@bereanchristian.com>
2019-11-22 10:44:10 -0800

    removed extends linearopmode Vuforia.java

--- Vuforia.java
+++ Vuforia.java
@@ -33,7 +33,7 @@ import static java.lang.Math.*;
 /*see  skystone/doc/tutorial/FTC_FieldCoordinateSystemDefinition.pdf
 */
  
-public class Vuforia extends LinearOpMode {
+public class Vuforia {
  
     // IMPORTANT:  For Phone Camera, set 1) the camera source and 2) the orientation, based on how your phone is mounted:
     // 1) Camera Source.  Valid choices are:  BACK (behind screen) or FRONT (selfie side)
