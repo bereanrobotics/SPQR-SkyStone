0e06f342388ae6884bc684fb870c26fb224b6aeb
Arkin Solomon <arkinsolomon@gmail.com>
2019-11-30 12:04:51 -0800

    Update HardwareSPQR.java

--- HardwareSPQR.java
+++ HardwareSPQR.java
@@ -12,9 +12,12 @@ import com.qualcomm.robotcore.hardware.Servo;
  * Motor channel:  Right back drive motor:       "right_back_drive"
  * Motor channel:  Left intake motor:            "left_intake"
  * Motor channel:  Right intake motor:           "right_intake"
+ * Motor channel:  Arm motor:                    "arm_motor"
  * Servo channel:  Left intake drop servo:       "left_drop"
  * Servo channel:  Right intake drop servo:      "right_drop"
  * Servo channel:  Block hitter servo:           "block_beater"
+ * Servo channel:  Block grabber servo:          "block_grabber"
+ * Servo channel:  Arm balancer servo:           "arm_balancer"
  */
 public class HardwareSPQR {
 
@@ -25,9 +28,12 @@ public class HardwareSPQR {
     public DcMotor rightBackDrive = null;
     public DcMotor leftIntake = null;
     public DcMotor rightIntake = null;
+    private DcMotor armMotor = null;
     public Servo leftDrop = null;
     public Servo rightDrop = null;
     public Servo blockBeater = null;
+    public Servo blockGrabber = null;
+    public Servo armBalancer = null;
 
     //Hardware map
     HardwareMap hwMap = null;
@@ -47,14 +53,7 @@ public class HardwareSPQR {
         this.rightBackDrive = this.hwMap.get(DcMotor.class, "right_back_drive");
         this.leftIntake = this.hwMap.get(DcMotor.class, "left_intake");
         this.rightIntake = this.hwMap.get(DcMotor.class, "right_intake");
-
-        //Set all motor power to zero
-        this.leftFrontDrive.setPower(0);
-        this.leftBackDrive.setPower(0);
-        this.rightFrontDrive.setPower(0);
-        this.rightBackDrive.setPower(0);
-        this.leftIntake.setPower(0);
-        this.rightIntake.setPower(0);
+        this.armMotor = this.hwMap.get(DcMotor.class, "arm_motor");
 
         //Set all motors to use or not use encoders
         this.leftFrontDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
@@ -63,6 +62,26 @@ public class HardwareSPQR {
         this.rightBackDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         this.leftIntake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         this.rightIntake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        this.armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER); //Reset encoder position to 0
+        this.armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        
+        //Sets motor direction
+        this.leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        this.leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
+        this.rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        this.rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
+        this.leftIntake.setDirection(DcMotor.Direction.REVERSE);
+        this.rightIntake.setDirection(DcMotor.Direction.FORWARD);
+        this.armMotor.setDirection(DcMotor.Direction.FORWARD);
+       
+        //Set all motor power to zero
+        this.leftFrontDrive.setPower(0);
+        this.leftBackDrive.setPower(0);
+        this.rightFrontDrive.setPower(0);
+        this.rightBackDrive.setPower(0);
+        this.leftIntake.setPower(0);
+        this.rightIntake.setPower(0);
+        this.armMotor.setPower(0);
 
         /* Initialize servos */
 
@@ -70,22 +89,12 @@ public class HardwareSPQR {
         this.leftDrop = this.hwMap.get(Servo.class, "left_drop");
         this.rightDrop = this.hwMap.get(Servo.class, "right_drop");
         this.blockBeater = this.hwMap.get(Servo.class, "block_beater");
+        this.blockGrabber = hardwareMap.get(Servo.class, "block_grabber");
+        this.armBalancer = hardwareMap.get(Servo.class, "arm_balancer");
 
         //Reset servo positions
         this.leftDrop.setPosition(1);
         this.rightDrop.setPosition(1);
         this.blockBeater.setPosition(1);
-    }
-
-    //Drop the intake
-    public void drop(){
-        this.leftDrop.setPosition(-1);
-        this.rightDrop.setPosition(-1);
-    }
-
-    //Reset intake position
-    public void resetIntakePosition(){
-        this.leftDrop.setPosition(1);
-        this.leftDrop.setPosition(1);
     }
 }
