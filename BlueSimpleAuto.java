57ed9925df2000fc436203a84cd954e22c4c72c9
BCHSPetersonOwen <petersonowen20@bereanchristian.com>
2019-12-07 15:56:18 -0800

    Update BlueSimpleAuto.java

--- BlueSimpleAuto.java
+++ BlueSimpleAuto.java
@@ -6,12 +6,15 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import java.lang.System.*;
 import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
+import static java.lang.Math.pow;
 
 @Autonomous(name="Blue Simple Auto")
 public class BlueSimpleAuto extends LinearOpMode {
     private boolean isInitialized = false;
     private HardwareSPQR robot = new HardwareSPQR();
-    private double nsPerS = 2000000000;
+    private double nsPerS = 1*pow(10, 9);
 
     private double timeStarted;
 
@@ -23,7 +26,7 @@ public class BlueSimpleAuto extends LinearOpMode {
         return elaspedTime;
     }
 
-    public void goForward (double time) { //go forward for
+    public void forwardTime(double time, double speed) { //go forward for
         updateTelemtry ();
         for(double startTime = getElaspedTime(timeStarted); (getElaspedTime(startTime) < time) && opModeIsActive();) {
             this.robot.setPowers(speed);
@@ -31,6 +34,22 @@ public class BlueSimpleAuto extends LinearOpMode {
         }
         this.robot.setPowers(0);
         updateTelemtry ();
+    }
+
+    public void turnTime(String direction, double time, double speed) {
+        updateTelemtry();
+        direction = direction.toLowerCase(Locale.ENGLISH);
+        for(double startTime = getElaspedTime(timeStarted); (getElaspedTime(startTime) < time) && opModeIsActive();) {
+            if (direction == "right") {
+                this.robot.tank(speed, -speed);
+            } else if (direction == "left") {
+                this.robot.tank(-speed, speed);
+            } else {
+                telemetry.addLine("Error: expected a [left or right] in the direction for turnTime and did not find it.");
+                telemetry.update();
+            }
+        }
+        this.robot.setPowers(0);
     }
 
     public void updateTelemtry () {
@@ -46,9 +65,10 @@ public class BlueSimpleAuto extends LinearOpMode {
         }
         waitForStart();
         if(opModeIsActive() && !isStopRequested()) {
-            timeStarted = System.nanoTime() / nsPerS;
+            timeStarted = System.nanoTime()/nsPerS;
             updateTelemtry ();
-            goForward(1);
+            forwardTime(1, speed);
+            turnTime("left", 1, speed);
             requestOpModeStop();
         }
     }
