f55b0ff530b11421f9037574d1b947475f47aaf5
4998 <arkinsolomon@gmail.com>
2019-12-07 18:16:01 -0800

    Did things

--- HardwareSPQR.java
+++ HardwareSPQR.java
@@ -54,6 +54,9 @@ public class HardwareSPQR {
     //True if robot is initialized
     private boolean robotIsInitialized = false;
 
+    //Levels of blocks
+    public static int[] levels = {-830, -1050, -1270};
+
     //Initializer
     public void init(HardwareMap ahwMap) {
 
@@ -87,7 +90,7 @@ public class HardwareSPQR {
         this.rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
         this.rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
         this.leftIntake.setDirection(DcMotor.Direction.FORWARD);
-        this.rightIntake.setDirection(DcMotor.Direction.FORWARD);
+        this.rightIntake.setDirection(DcMotor.Direction.REVERSE);
         this.armMotor.setDirection(DcMotor.Direction.FORWARD);
        
         //Set all motor power to zero
@@ -192,5 +195,27 @@ public class HardwareSPQR {
     public void stopIntake(){
         this.leftIntake.setPower(0);
         this.rightIntake.setPower(0);
+    }
+
+    /* Arm movement */
+
+    //Drop the arm down to levels
+    public void moveArm(int level, double speed){
+        this.armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        this.armMotor.setTargetPosition(this.levels[level]);
+        this.armMotor.setPower(speed);
+        while (this.armMotor.isBusy()){
+            this.armMotor.setPower(0);
+        }
+    }
+
+    //Grab a block
+    public void grabBlock(){
+        this.blockGrabber.setPosition(-1);
+    }
+
+    //Release a block
+    public void releaseBlock(){
+        this.blockGrabber.setPosition(1);
     }
 }
---
+++ SPQRLinearOpMode.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+/**
+ * Custom Linear OpMode class with extra functions
+ */
+public abstract class SPQRLinearOpMode extends LinearOpMode {
+
+    public HardwareSPQR robot = new HardwareSPQR();
+
+    public void hardwareInit(){
+        this.robot.init(hardwareMap);
+    }
+
+    public void driveForTime(double speed, long milliseconds){
+        this.robot.setPowers(speed);
+        this.sleep(milliseconds);
+        this.robot.setPowers(0);
+    }
+
+    public void turnForTime(Dir direction, double speed, long milliseconds){
+        if (direction == Dir.RIGHT) {
+            this.robot.tank(speed, -speed);
+        } else {
+            this.robot.tank(-speed, speed);
+        }
+    }
+}
