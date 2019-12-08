613b7f54b6f34c2663b81f6f02c004e12430b9bc
BCHSPetersonOwen <petersonowen20@bereanchristian.com>
2019-12-08 14:32:36 -0800

    updateing arking
    Co-Authored-By: Arkin Solomon <arkinsolomon@users.noreply.github.com>

--- BlueAutoTime.java
+++ BlueAutoTime.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous (name="Blue Auto Time")
 public class BlueAutoTime extends SPQRLinearOpMode {
 
 
@@ -9,8 +12,14 @@ public class BlueAutoTime extends SPQRLinearOpMode {
 
         waitForStart();
 
-        this.driveForTime(this.speed, 550);
+        this.driveForTime(this.speed, 100);
+        this.turnForTime(Dir.LEFT, this.speed, 800);
+        this.driveForTime(this.speed, 800);
+        this.turnForTime(Dir.RIGHT, this.speed, 800);
+        this.driveForTime(this.speed, 500);
         this.robot.tow.setPosition(1);
+        this.sleep(1000);
+        this.driveForTime(-this.speed, 1750);
     }
 }
 
