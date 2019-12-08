39106e4ac1143b80ce0602668a53af65d2019e92
BCHSPetersonOwen <petersonowen20@bereanchristian.com>
2019-12-08 09:13:46 -0800

    Need to use other laptop

--- BlueSimpleAuto.java
+++ BlueSimpleAuto.java
@@ -20,29 +20,53 @@ public class BlueSimpleAuto extends SPQRLinearOpMode {
 
         if (opModeIsActive() && !isStopRequested()) {
             this.driveForTime(this.speed, justSpeed(250, this.speed));
+
             this.turnForTime(Dir.LEFT, this.turnSpeed, justSpeed(this.ninetyDegreeTime, this.turnSpeed));
+
             this.driveForTime(this.speed, justSpeed(3250, this.speed));
+
             this.turnForTime(Dir.RIGHT, this.turnSpeed, justSpeed(this.ninetyDegreeTime, this.turnSpeed));
+
             this.driveForTime(this.speed, this.justSpeed(550, this.speed));
+
             this.robot.tow.setPosition(1);
+
             this.sleep(1000);
+
             this.driveForTime(-this.speed, this.justSpeed(1000, this.speed));
+
             this.robot.tow.setPosition(0);
+
             this.turnForTime(Dir.RIGHT, this.turnSpeed, justSpeed(this.ninetyDegreeTime, this.turnSpeed));
+
             this.driveForTime(this.speed, this.justSpeed(1400, this.speed));
+
             this.turnForTime(Dir.LEFT, this.turnSpeed, justSpeed(this.ninetyDegreeTime, this.turnSpeed));
+
             this.driveForTime(speed, this.justSpeed(1000, this.speed));
+
             this.turnForTime(Dir.LEFT, this.turnSpeed, justSpeed(this.ninetyDegreeTime, this.turnSpeed));
+
             this.driveForTime(this.speed, this.justSpeed(1400, this.speed));
+
             this.turnForTime(Dir.LEFT, this.turnSpeed, justSpeed(this.ninetyDegreeTime, this.turnSpeed));
+
             this.driveForTime(this.speed, this.justSpeed(400, this.speed));
+
             this.driveForTime(-this.speed, this.justSpeed(250, this.speed));
+
             this.turnForTime(Dir.LEFT, this.turnSpeed, justSpeed(this.ninetyDegreeTime, this.turnSpeed));
+
             this.driveForTime(this.speed, justSpeed(900, this.speed));
+
             this.turnForTime(Dir.RIGHT, this.turnSpeed, justSpeed(this.ninetyDegreeTime, this.turnSpeed));
+
             this.driveForTime(this.speed, justSpeed(400, this.speed));
+
             this.turnForTime(Dir.LEFT, this.turnSpeed, justSpeed(this.ninetyDegreeTime, this.turnSpeed));
+
             this.driveForTime(speed, justSpeed(300, speed));
+
             if (true) {
                 this.robot.setPowers(0);
             }
