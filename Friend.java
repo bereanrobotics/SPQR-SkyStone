ca79fd0b13cc148e7625fc3cd21149e5f366ef54
BCHSPetersonOwen <petersonowen20@bereanchristian.com>
2020-02-08 09:39:16 -0800

    Added friend and York (to start the robot and go for a block at the very end)

---
+++ Friend.java
@@ -0,0 +1,58 @@
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+/**
+ * Autonomous: BLUE ALLIANCE
+ *
+ * Start against the wall in such a way that the right edge of the robot is next to the red depot.
+ *
+ * Steps:
+ *  - Grab a block.
+ *  - Deliver the block.
+ *  - Park in the outside lane on the line under the blue bridge.
+ *
+ * @author Owen Peterson
+ */
+@Autonomous(name="4 'Friend' Red -bot -bot (1) Block (2) Line -out", group="Red")
+public class Friend extends SPQRLinearOpMode {
+
+    @Override
+    public void runOpMode() {
+        this.hardwareInit();
+
+        waitForStart();
+
+        if (opModeIsActive() && !isStopRequested()) {
+
+            //Initial movement forward
+            this.drive(7000, 1);
+
+            //grab the block using the tow, wait for the tow to come down before
+            this.robot.dropTow();
+            this.sleep(1000);
+
+            //reverse away from the blocks
+            this.drive(-3750, -1);
+
+            //turn right towards the top of the field
+            this.turn(-90, 1.0);
+
+            //drive across the line
+            this.drive(17000, 1.0);
+
+            //releasing the block, with a slight pause to make sure it is clear of the tow
+            this.robot.raiseTow();
+            this.sleep(1000);
+
+            //reverse back to ensure line up.
+            this.drive(-5000, -1);
+
+            //strafe down onto the line
+            this.strafe(Dir.RIGHT, 5000, 1);
+
+
+        }
+    }
+}
